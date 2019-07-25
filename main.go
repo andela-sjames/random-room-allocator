138d4c735ec569e5e17d93c2aefa3b11e3f04820
andela-sjames <samuel.james@andela.com>
2019-07-25 01:36:36 +0100

    Set basic structure.

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 
 // Employee struct defined
 type employee struct {
-	name        string
-	gender      string
-	position    string
-	wantsLiving bool
+	name     string
+	gender   string
+	position string
+	optSpace bool
 }
 
 func closeFile(f *os.File) {
@@ -28,15 +28,49 @@ func closeFile(f *os.File) {
 func generateObject(f *os.File) {
 	fmt.Println("generating data object")
 
+	employees := make(map[string][]map[string]string)
+
+	var fellowSlice []map[string]string
+	var staffSlice []map[string]string
+
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := strings.Fields(scanner.Text())
-		fmt.Printf("%q\n", line[0])
+		if line[2] == "STAFF" {
+			staffMap := make(map[string]string)
+			staffMap["name"] = line[0]
+			staffMap["gender"] = line[1]
+			staffMap["position"] = line[2]
+
+			staffSlice = append(staffSlice, staffMap)
+		}
+
+		if line[2] == "FELLOW" {
+			fellowsMap := make(map[string]string)
+			optSpace := "true"
+			if line[3] == "Y" {
+				optSpace = "false"
+			}
+
+			fellowsMap["name"] = line[0]
+			fellowsMap["gender"] = line[1]
+			fellowsMap["position"] = line[2]
+			fellowsMap["optSpace"] = optSpace
+
+			fellowSlice = append(fellowSlice, fellowsMap)
+		}
+
+		// fmt.Printf("%q\n", line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
+
+	employees["staff"] = staffSlice
+	employees["fellow"] = fellowSlice
+
+	fmt.Printf("%q\n", employees)
 }
 
 // FileParser gets data from inputfile.
@@ -58,4 +92,5 @@ func (fp *fileParser) GetEmployees() {
 func main() {
 	inputfile := &fileParser{filepath: "inputA.txt"}
 	inputfile.GetEmployees()
+	// fmt.Printf("%q\n", employees)
 }
